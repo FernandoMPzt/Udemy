8477e11daf71956f70ce0a48233bcfe834efe0f3
Fernando Mota <fer.nandomota@outlook.com>
2022-04-30 12:35:55 -0300

    filter,map.reduce

---
+++ CursoJavaCompleto/lambdas/StreamAPI/List/Stream/Aluno.java
@@ -0,0 +1,14 @@
+
+public class Aluno {
+
+  final double nota;
+  final String nome;
+  final String genero;
+
+  public Aluno(String nome, double nota, String genero){
+    this.nome = nome;
+    this.nota= nota;
+    this.genero = genero;
+  }
+  
+}
